c957ea320cae82fdacf09481b50cc5fa801bcd6c
Patrick-BP <papybp@gmail.com>
2023-12-13 10:41:52 -0700

    Glab 303.3.1

---
+++ LAB 303.3.1 -  Java String Methods/src/com/JavaStringMethods/StringTrimExample.java
@@ -0,0 +1,10 @@
+package com.JavaStringMethods;
+
+public class StringTrimExample {
+    public static void main(String args[]){
+        String s1="  hello   ";
+        System.out.println(s1+"how are you");
+        System.out.println(s1.trim()+"how are you");
+        }
+
+    }
