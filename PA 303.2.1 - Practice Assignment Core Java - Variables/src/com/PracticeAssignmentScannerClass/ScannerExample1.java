442d4ce4103e00aec2e7cb6adcc169c7ceb3350d
Patrick-BP <papybp@gmail.com>
2023-12-14 20:14:40 -0700

    ScannerExample

--- PA 303.2.1 - Practice Assignment Core Java - Variables/src/com/PracticeAssignmentScannerClass/ScannerExample1.java
+++ PA 303.2.1 - Practice Assignment Core Java - Variables/src/com/PracticeAssignmentScannerClass/ScannerExample1.java
@@ -1,14 +1,12 @@
 package com.PracticeAssignmentScannerClass;
 
 import java.util.Scanner;
-
 public class ScannerExample1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
-
 
         System.out.println(a);
         System.out.println(b);
