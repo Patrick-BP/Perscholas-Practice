7f153361003775e2f3b5a1ab112f260c9bd429f4
Patrick-BP <papybp@gmail.com>
2024-01-30 10:50:15 -0700

    fhg

---
+++ GLAB-305.4.1A-Hibernate Project Demonstration/src/main/java/App.java
@@ -0,0 +1,15 @@
+import com.test.hib.controller.findUser_Hql;
+
+public class App {
+    public static void main( String[] args )
+    {
+        findUser_Hql u = new findUser_Hql();
+        //u.findUser();
+//        u.findUserSelect();
+        //u.getRecordbyId();
+        //u.getrecords();
+        //u.getmaxSalary();
+        u.getmaxSalaryGroupBy();
+    }
+
+}
