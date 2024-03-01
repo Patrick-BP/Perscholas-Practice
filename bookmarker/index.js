02c40d601b59e9f44d91263a78e89286cb650856
Patrick-BP <papybp@gmail.com>
2024-03-01 00:32:06 -0700

    sdfsdfs

--- bookmarker/index.js
+++ bookmarker/index.js
@@ -1,4 +1,4 @@
-const users = [{id:1, email:"test@gmail.com", password:"123"}, {id:2, email:"test2@gmail.com", password:"123"}]
+
 
 const loginform = document.getElementById("loginform");
 const message = document.getElementById("message")
@@ -8,7 +8,7 @@ loginform.addEventListener("submit",(e)=>{
      e.preventDefault();
     const email = loginform.email.value;
     const pass = loginform.password.value;
-    if (email === "test@gmail.com" && pass === "123") {
+    if (email === "test@gmail.com" && pass === "1aA") {
         window.location = "home.html"
     } else {
         message.style.color ="red"
